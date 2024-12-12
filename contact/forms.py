76ddc2da9708b525dbdc77b9dcc37086f99e5a64
Bruno Machado <brunobbmachado97@outlook.com>
2024-12-12 09:29:58 -0300

    form2

---
+++ contact/forms.py
@@ -0,0 +1,7 @@
+from contact.models import Contact
+from django import forms
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('first_name','last_name','phone',)
